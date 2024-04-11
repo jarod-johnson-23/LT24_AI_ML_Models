87fd1942648b88f51f6df77fa2855da62e4ae353
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2024-04-11 16:01:33 -0700

    initial whisper model

---
+++ app.py
@@ -0,0 +1,46 @@
+import whisper
+from flask import Flask, request, jsonify
+import os
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(
+    app,
+    resources={r"/*": {"origins": "https://py.laneterraleverapi.org"}},
+    supports_credentials=True,
+)
+
+# Load the Whisper model
+model = whisper.load_model("large")  # Choose between "tiny", "base", "small", "medium", "large" based on your needs and resources
+
+@app.route("/whisper_asr", methods=["POST"])
+def perform_asr():
+  # Check if the post request has the file part
+  if 'audio_file' not in request.files:
+      return jsonify({'error': 'No file part'}), 400
+  file = request.files['audio_file']
+
+  if file.filename == '':
+      return jsonify({'error': 'No selected file'}), 400
+
+  if file and file.filename.lower().endswith('.mp3'):      
+      audio_data = file.read()
+      result_segments = model.transcribe(audio_data)
+
+      transcription_details = []
+
+      for segment in result_segments["segments"]:
+          start_time = segment['start']
+          end_time = segment['end']
+          text = segment['text']
+
+          transcription_details.append({
+              'start_time': start_time,
+              'end_time': end_time,
+              'text': text
+          })
+
+      return jsonify(transcription_details)
+  else:
+    return jsonify({'error': 'Other General Error'}), 400
